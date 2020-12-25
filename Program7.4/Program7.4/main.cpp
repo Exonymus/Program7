b375576d9f9f2ab9f5b46984b687719f58baa995
Igor <exonymus@icloud.com>
2020-12-25 14:27:40 +0300

    Update

--- Program7.4/Program7.4/main.cpp
+++ Program7.4/Program7.4/main.cpp
@@ -10,83 +10,112 @@
 
 using namespace std;
 
-int to_ten(string num);
-string num_plus(int num1, int num2);
-string num_minus(int num1, int num2);
+string num_plus(string num1, string num2);
+string num_minus(string num1, string num2);
+
 
 int main()
 {
     string num1, num2, result;
-    int num_1, num_2;
     char symbol;
     cout << "Введите 1 число: ";
     cin >> num1;
-    num_1 = to_ten(num1);
     
     cout << "Введите 2 число: ";
     cin >> num2;
-    num_2 = to_ten(num2);
     
     cout << "Введите операцию(+/-): ";
     cin >> symbol;
     
-    if (symbol == '+') result = num_plus(num_1, num_2);
-    if (symbol == '-') result = num_minus(num_1, num_2);
+    if (symbol == '+') result = num_plus(num1, num2);
+    if (symbol == '-') result = num_minus(num1, num2);
+   
     
     cout <<"Полученное число: " << result << endl;
     return 0;
 }
 
-int to_ten(string num)
+string num_plus(string num1, string num2)
 {
-    int num2 = 0;
-    for (int i = 0; i < num.length(); i++)
-    {
-        if (num[i] != 'a' && num[i] != 'b') num2 += ((int) num[i] - 48) * pow(12,(num.length()-i-1));
-        if (num[i] == 'a') num2 += 10 * pow(12,(num.length()-i-1));
-        if (num[i] == 'b') num2 += 11 * pow(12,(num.length()-i-1));
-    }
-    return num2;
-}
-
-string num_plus(int num1, int num2)
-{
-    int num = num1 + num2;
     string sum;
-    for (int i = 0; num != 0; i++)
-    {
-        int k = num % 12;
-        if (k < 10) sum += to_string(k);
-        else if (k == 10) sum += "a";
-        else if (k == 11) sum += "b";
-        num = num / 12;
-    }
+    long n = 0;
     
-    long n = sum.length();
-    for (int i = 0; i < n / 2; i++)
-        swap(sum[i], sum[n - i - 1]);
+    if (num1.length() > num2.length())
+        while (num1.length() > num2.length())
+            num2 = '0' + num2;
+    else if (num1.length() < num2.length())
+        while (num2.length() > num1.length())
+            num1 = '0' + num1;
+   
+    n = num1.length() - 1;
+    
+    int nsum = 0;
+    bool check = false;
+    for (long i = n; i >= 0; i--)
+    {
+        if (num1[i] != 'a' && num1[i] != 'b') nsum += (int) num1[i] - 48;
+        if (num2[i] != 'a' && num2[i] != 'b') nsum += (int) num2[i] - 48;
+        
+        if (num1[i] == 'a') nsum += 10;
+        if (num2[i] == 'a') nsum += 10;
+        
+        if (num1[i] == 'b') nsum += 11;
+        if (num2[i] == 'b') nsum += 11;
+        
+        if (nsum == 10) sum = 'a' + sum;
+        else if (nsum == 11) sum = 'b' + sum;
+        else if (nsum < 10) sum = to_string(nsum) + sum;
+        
+        if (nsum > 11 && (nsum-12) < 10) {sum = to_string(nsum - 12) + sum; check = true;}
+        else if (nsum > 11 && (nsum-12) == 10) {sum = 'a' + sum; check = true;}
+        else if (nsum > 11 && (nsum-12) == 11) {sum = 'b' + sum; check = true;}
+        
+        if ((nsum - 12) < 0) check = false;
+        if (check == true) nsum = 1; else nsum = 0;
+    }
+    if (nsum == 1) sum = '1' + sum;
     return sum;
 }
 
-string num_minus(int num1, int num2)
+string num_minus(string num1, string num2)
 {
-    int num;
     string msum;
     if (num1 == num2) {msum = '0'; return msum;}
-    if (num1 > num2) num = num1 - num2;
-        else num = num2 - num1;
     
-    for (int i = 0; num != 0; i++)
+    long n = 0;
+    
+    if (num1.length() > num2.length())
+        while (num1.length() > num2.length())
+            num2 = '0' + num2;
+    else if (num1.length() < num2.length())
+        while (num2.length() > num1.length())
+            num1 = '0' + num1;
+   
+    n = num1.length() - 1;
+    
+    int nsum = 0;
+    bool check = false;
+    for (long i = n; i >= 0; i--)
     {
-        int k = num % 12;
-        if (k < 10) msum += to_string(k);
-        else if (k == 10) msum += "a";
-        else if (k == 11) msum += "b";
-        num = num / 12;
+        if (num1[i] != 'a' && num1[i] != 'b') nsum += (int) num1[i] - 48;
+        if (num2[i] != 'a' && num2[i] != 'b') nsum -= (int) num2[i] - 48;
+        
+        if (num1[i] == 'a') nsum += 10;
+        if (num2[i] == 'a') nsum -= 10;
+        
+        if (num1[i] == 'b') nsum += 11;
+        if (num2[i] == 'b') nsum -= 11;
+
+        if (nsum > 0) check = false;
+        if (nsum < 0) {nsum += 12; check = true;}
+        
+        if (nsum == 10) msum = 'a' + msum;
+        else if (nsum == 11) msum = 'b' + msum;
+        else if (nsum < 10 && nsum >= 0) msum = to_string(nsum) + msum;
+        
+        
+        if (check == true) nsum = -1; else nsum = 0;
     }
-    
-    long n = msum.length();
-    for (int i = 0; i < n / 2; i++)
-        swap(msum[i], msum[n - i - 1]);
     return msum;
 }
+
